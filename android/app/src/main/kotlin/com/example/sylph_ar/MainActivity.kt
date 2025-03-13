67446f76b60a9df6d62dd0101df61d64cc00aeb4
Nanda maulana <>
2025-03-13 19:06:18 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/sylph_ar/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sylph_ar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/theme.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color primaryColor = const Color.fromARGB(255, 149, 1, 1);
+Color secondaryColor = const Color.fromARGB(255, 196, 79, 112);
+Color whiteColor = const Color.fromARGB(255, 241, 241, 241);
+Color backGround = const Color.fromARGB(255, 255, 255, 255);
+Color blackColor = const Color.fromARGB(255, 18, 18, 18);
+Color ternaryColor = const Color.fromARGB(255, 255, 133, 164);
+Color addColor = const Color.fromARGB(255, 255, 188, 219);
+Color textColor = const Color(0xff6C6464);
+
+double defaultMargin = 24.0;
+
+TextStyle bigTitleStyle = GoogleFonts.lato(
+  color: blackColor,
+  fontSize: 24,
+  fontWeight: FontWeight.bold,
+);
+TextStyle titleStyle = GoogleFonts.lato(
+  color: blackColor,
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+);
+TextStyle mediumTextStyle = GoogleFonts.lato(
+  color: textColor,
+  fontSize: 12,
+  fontWeight: FontWeight.w500,
+);
+TextStyle semiBoldTextStyle = GoogleFonts.lato(
+  color: textColor,
+  fontSize: 12,
+  fontWeight: FontWeight.w700,
+);
+TextStyle semiBoldTextStyle16 = GoogleFonts.lato(
+  color: textColor,
+  fontSize: 16,
+  fontWeight: FontWeight.w700,
+);
+TextStyle semiBoldTextStyle16white = GoogleFonts.lato(
+  color: whiteColor,
+  fontSize: 16,
+  fontWeight: FontWeight.w700,
+);
+
+TextStyle regularTextStyle = GoogleFonts.lato(
+  color: textColor,
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+);
+TextStyle regularTextStylePink = GoogleFonts.poppins(
+  color: secondaryColor,
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+);
+TextStyle bigButtonTextStyle = GoogleFonts.lato(
+  color: whiteColor,
+  fontSize: 18,
+  fontWeight: FontWeight.w500,
+);
+TextStyle smallButtonTextStyle = GoogleFonts.lato(
+  color: whiteColor,
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+);
